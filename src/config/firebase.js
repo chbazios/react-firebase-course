676526471a1ae8fd9bbf52d7580947693563894e
Christos Bazios <chbazios78@gmail.com>
2023-04-18 21:16:36 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -14,13 +14,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBL4FHJN_olcz-JnW0CprGQgZZbIPX5BRw",
-  authDomain: "fir-course-2023a.firebaseapp.com",
-  projectId: "fir-course-2023a",
-  storageBucket: "fir-course-2023a.appspot.com",
-  messagingSenderId: "912001322617",
-  appId: "1:912001322617:web:139827a38cedeef3370ea8",
-  measurementId: "G-YX1XFC6451",
+
+  
+  
+  HERE YOU ADD YOUR CONFIGURATION
+  
+  
+  
+  
 };
 
 // Initialize Firebase
